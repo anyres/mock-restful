a96a68a4ea96d4d054d32470042ddb6f4e71859d
niuba <niuba.zbz@gmail.com>
2018-10-08 17:50:17 +0800

    v0.0.1
    Signed-off-by: niuba <niuba.zbz@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import * as MOCKDATA from '../../_mock';
+import { DelonMockModule } from '@delon/mock';
+import { HttpClientModule } from '@angular/common/http';
+import { UserService } from './user.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    DelonMockModule.forRoot({ data: MOCKDATA })
   ],
-  providers: [],
+  providers: [
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
